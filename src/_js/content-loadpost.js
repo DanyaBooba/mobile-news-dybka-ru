2307e0c259599768fdf26474933b70726546c386
Daniil Dybka <creagoooff@gmail.com>
2023-07-02 16:32:38 +0300

    Add new posts

--- src/_js/content-loadpost.js
+++ src/_js/content-loadpost.js
@@ -1,19 +1,72 @@
 var content = document.getElementById("content");
-var children = document.getElementById("content").children;
+// var children = document.getElementById("content").children;
 var count = 1;
 var max = 5;
 
+var array = [
+	"airpods-pro-is-it-worth-the-money",
+	"babkaonthehuntclassic",
+	"change-creagoo-ru-simplify-improve-and-share",
+	"communication-with-clients-on-the-internet",
+	"difference-in-macbooks-for-3-years",
+	"experience-using-iphone-6s-for-six-months",
+	"great-news-channel-special",
+	"how-did-i-start-learning-programming",
+	"how-i-develop-websites-in-php",
+	"how-i-fight-against-burnout",
+	"how-to-sell-on-buy-markets",
+	"ipad-mini-4-is-an-ambiguous-device",
+	"macbook-2017-review",
+	"macbook-pro-2014-13-review",
+	"news-archive-share-design-update",
+	"novyy-sayt-creagoo-ru",
+	"pet-project-development-what-is-it-and-for-what",
+	"php-is-alive",
+	"project-development-history-live-creagoo-ru-its-forced-closure",
+	"rememberthesecards",
+	"rockpaperscissors",
+	"sweetness",
+	"the-path-of-a-programmer-is-a-difficult-path",
+	"used-market-what-is-it-with-what-it-is-eaten",
+	"which-macbooks-are-really-worth-getting",
+];
+
 function MorePosts() {
 	if (count < max) {
-		var block = document.createElement("div");
-		block.classList.add("post--after");
-		var str = "";
-		for (i = 0; i < children.length; i++) {
-			str += children[i].outerHTML;
-		}
+		(function () {
+			var http = new XMLHttpRequest();
+			var link = "airpods-pro-is-it-worth-the-money";
 
-		block.innerHTML = str;
-		content.after(block);
+			http.open("GET", "/articles/" + link);
+			http.onreadystatechange = function () {
+				if (this.readyState == 4 && this.status == 200) {
+					var dom = new DOMParser()
+						.parseFromString(this.responseText, "text/html")
+						.getElementById("content").children;
+
+					if (dom !== null) {
+						// console.log(dom);
+
+						var block = document.createElement("div");
+						block.classList.add("post--after");
+						var str = "";
+						for (i = 0; i < dom.length; i++) {
+							if (dom[i].children.length > 0) {
+								if (dom[i].children[0].tagName === "IMG") {
+									dom[i].children[0].src = "/articles/" + link + "/cap.jpg";
+								}
+							}
+
+							str += dom[i].outerHTML;
+						}
+
+						block.innerHTML = str;
+						content.after(block);
+					}
+				}
+			};
+			http.send(null);
+		})();
 
 		count += 1;
 	}
